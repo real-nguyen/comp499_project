1fd1dd347682ebd153ce29660e9242d88fead2e8
Real Nguyen <real.nguyen@hotmail.com>
2019-03-21 22:48:46 -0400

    - Part 1 outputs are now grayscale images with points representing each feature - Store Rainier images in a list and get their gradients in a loop

--- project.py
+++ project.py
@@ -6,32 +6,18 @@ from os.path import join
 
 DIRNAME = dirname(__file__)
 DIR_IMG = join(DIRNAME, 'project_images')
-R_THRESHOLD = 0.000255
 boxes = cv2.imread(join(DIR_IMG, 'Boxes.png'))
 boxes_gray = cv2.cvtColor(boxes,cv2.COLOR_BGR2GRAY)
 boxes_gray = np.float32(boxes_gray)
-# rainier1_orig = cv2.imread(join(DIR_IMG, 'Rainier1.png'))
-# rainier2_orig = cv2.imread(join(DIR_IMG, 'Rainier2.png'))
-# rainier3_orig = cv2.imread(join(DIR_IMG, 'Rainier3.png'))
-# rainier4_orig = cv2.imread(join(DIR_IMG, 'Rainier4.png'))
-# rainier5_orig = cv2.imread(join(DIR_IMG, 'Rainier5.png'))
-# rainier6_orig = cv2.imread(join(DIR_IMG, 'Rainier6.png'))
-
-def detect_features(img, Ix, Iy):
-    Ix2 = np.matrix(Ix * Ix, dtype=np.float32)
-    Iy2 = np.matrix(Iy * Iy, dtype=np.float32)
-    IxIy = np.matrix(Ix * Iy, dtype=np.float32)
-    keypoints = img.copy()
-    keypoints[:] = 0
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            det = Ix2[i, j] * Iy2[i, j] - (IxIy[i, j])**2
-            trace = Ix2[i, j] + Iy2[i, j]
-            R = det / trace if trace != 0 else 0
-            if R > R_THRESHOLD:
-                keypoints[i, j] = img[i, j]
-    local_max = get_local_max(keypoints)
-    return local_max
+rainier_lst = []
+rainier_gray_lst = []
+rainier_Ix = []
+rainier_Iy = []
+for i in range(6):
+    rainier_lst.append(cv2.imread(join(DIR_IMG, f'Rainier{i + 1}.png')))
+    rainier_gray_lst.append(np.float32(cv2.cvtColor(rainier_lst[i], cv2.COLOR_BGR2GRAY)))
+    rainier_Ix.append(cv2.Sobel(rainier_gray_lst[i], cv2.CV_32F, 1, 0, ksize=3))
+    rainier_Iy.append(cv2.Sobel(rainier_gray_lst[i], cv2.CV_32F, 0, 1, ksize=3))
 
 def get_local_max(img):
     # Returns a black image with dots representing the features
@@ -60,17 +46,54 @@ def get_keypoints(features):
     for y in range(features.shape[0]):
         for x in range(features.shape[1]):
             if features[y, x] > 0.01*features.max():
-                keypoints.append(cv2.KeyPoint(x, y, 3))
+                keypoints.append(cv2.KeyPoint(x, y, 1))
     return keypoints
 
-features = cv2.cornerHarris(boxes_gray,3,3,0.04)
-# features = cv2.dilate(features, None)
-features = get_local_max(features)
-kp = get_keypoints(features)
-sift = cv2.xfeatures2d.SIFT_create()
-descriptors = sift.compute(boxes,  None)
-boxes_kp = boxes.copy()
-img = cv2.drawKeypoints(boxes, kp, boxes_kp)
-cv2.imshow('detected keypoints', img)
+def get_out_img(features):
+    out = np.zeros(features.shape)
+    for y in range(features.shape[0]):
+        for x in range(features.shape[1]):
+            if features[y, x] > 0.01*features.max():
+                # Map feature intensity to 0-255 values, so that the features can be visualised
+                out[y, x] = math.floor((features[y,x]*255) / features.max())
+    return out
+
+# Get image gradients
+boxes_Ix = cv2.Sobel(boxes_gray, cv2.CV_32F, 1, 0, ksize=3)
+boxes_Iy = cv2.Sobel(boxes_gray, cv2.CV_32F, 0, 1, ksize=3)
+
+boxes_features = cv2.cornerHarris(boxes_gray, 3, 3, 0.04)
+boxes_features = get_local_max(boxes_features)
+boxes_kp = get_keypoints(boxes_features)
+boxes_out = get_out_img(boxes_features)
+cv2.imwrite(join(DIR_IMG, '1a.png'), boxes_out)
+
+r1 = rainier_lst[0]
+r1_features = cv2.cornerHarris(rainier_gray_lst[0], 3, 3, 0.04)
+r1_features = get_local_max(r1_features)
+r1_kp = get_keypoints(r1_features)
+r1_out = get_out_img(r1_features)
+cv2.imwrite(join(DIR_IMG, '1b.png'), r1_out)
+
+r2 = rainier_lst[1]
+r2_features = cv2.cornerHarris(rainier_gray_lst[1], 3, 3, 0.04)
+r2_features = get_local_max(r2_features)
+r2_kp = get_keypoints(r2_features)
+r2_out = get_out_img(r2_features)
+cv2.imwrite(join(DIR_IMG, '1c.png'), r2_out)
+
+# sift = cv2.xfeatures2d.SIFT_create()
+# _, r1_desc = sift.compute(r1, r1_kp)
+# _, r2_desc = sift.compute(r2, r2_kp)
+# bf = cv2.BFMatcher()
+# matches = bf.match(r1_desc, r2_desc)
+# Apply ratio test
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+# matched_img = cv2.drawMatches(r1, r1_kp, r2, r2_kp, matches, None)
+# cv2.imshow('matched_img', matched_img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
